53f1da8e7e4603e9a559b78c20e3856f5ce76c84
Abhishek <abhishekkhatua19@gmail.com>
2018-09-26 18:00:31 +0530

    initial  CommitTwo

--- app/src/main/java/in/co/ngcapp/databinding/ImageAdapter.kt
+++ app/src/main/java/in/co/ngcapp/databinding/ImageAdapter.kt
@@ -1,19 +1,23 @@
 package `in`.co.ngcapp.databinding
 
+import `in`.co.ngcapp.databinding.R.id.thumbnail
 import `in`.co.ngcapp.databinding.databinding.ImageAdapterBinding
+import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
-class ImageAdapter(var userList:ArrayList<UserData>) : RecyclerView.Adapter<ImageAdapter.Clickevents>() {
+class ImageAdapter(var userList:ArrayList<User>,var mContext:Context) : RecyclerView.Adapter<ImageAdapter.Clickevents>() {
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageAdapter.Clickevents {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val binding : ImageAdapterBinding = DataBindingUtil.inflate(layoutInflater, R.layout.image_adapter, parent, false)        return  Clickevents(v)
+        var binding : ImageAdapterBinding = DataBindingUtil.inflate(layoutInflater, R.layout.image_adapter, parent, false)
 
         return  Clickevents(binding)
     }
@@ -26,9 +30,11 @@ class ImageAdapter(var userList:ArrayList<UserData>) : RecyclerView.Adapter<Imag
         holder.getData(userList[position])
     }
 
-    class Clickevents(var view:ImageAdapterBinding) : RecyclerView.ViewHolder(view.root)  {
+    class Clickevents(var imageBinding:ImageAdapterBinding) : RecyclerView.ViewHolder(imageBinding.root)  {
 
-        fun getData(userList:UserData){
+        fun getData(userList:User){
+
+            imageBinding.(userList.imagesUrl)
 
         }
     }
